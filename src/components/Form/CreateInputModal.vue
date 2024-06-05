c1b020de23b4ee9c5b8703d609a78bb16a6d661c
Diego <dosorio55@gmail.com>
2024-06-05 08:48:25 +0200

    modal added and working on it

---
+++ src/components/Form/CreateInputModal.vue
@@ -0,0 +1,37 @@
+<template>
+  <Modal
+    v-if="showModal"
+    :buttons="[
+      { text: 'OK', action: handleModalAction },
+      { text: 'ID', action: handleModalAction },
+    ]"
+    :close-modal="closeModal"
+    decline-button="cancel"
+  >
+    <DropdownInput :menu-items="menuItems"  />
+  </Modal>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import Modal from "../ui/Modal.vue";
+import DropdownInput from "../ui/form/DropdownInput.vue";
+
+const showModal = ref(true);
+
+const menuItems = [
+  { name: "Simple Input" },
+  { name: "Profile" },
+  { name: "Logout" },
+];
+
+const handleModalAction = () => {
+  console.log("Modal action");
+};
+
+const closeModal = () => {
+  showModal.value = false;
+};
+</script>
+
+<style scoped></style>
--- src/components/Form/FomMain.vue
+++ src/components/Form/FomMain.vue
@@ -3,11 +3,13 @@
     class="h-full overflow-auto p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700"
   >
     <FormSwitcher />
-    <FormPlusButton icon-name="Plus"  />
+    <FormPlusButton icon-name="Plus" />
+    <CreateInputModal />
   </div>
 </template>
 
 <script setup lang="ts">
+import CreateInputModal from "./CreateInputModal.vue";
 import FormPlusButton from "./FormPlusButton.vue";
 import FormSwitcher from "./FormSwitcher.vue";
 </script>
